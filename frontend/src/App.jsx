9dd0064449cf9bb8668cfc7deccffab2e38f0688
Saipreety Sutar <157683081+saipreetysutar123@users.noreply.github.com>
2025-10-23 12:01:54 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
+import UserProvider from './context/UserContext'
+import Dashboard from './pages/Dashboard'
+import { Toaster } from 'react-hot-toast'
+import EditResume from './components/EditResume'
+import AuthSuccess from './pages/AuthSuccess'
+import Login from './components/Login'
 
 const App = () => {
   return (
-    // <UserProvider>
+    <UserProvider>
       <Routes>
         <Route path='/' element={<LandingPage />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/resume/:resumeId' element={<EditResume />} />
+        <Route path='/auth-success' element={<AuthSuccess/>} />
+        <Route path='/login' element={<Login/>} />
       </Routes>
-    // {/* </UserProvider> */}
+
+      <Toaster toastOptions={{
+        className: "",
+        style: {
+          fontSize: "13px"
+        }
+      }}>
+
+
+      </Toaster>
+    </UserProvider>
   )
 }
 
